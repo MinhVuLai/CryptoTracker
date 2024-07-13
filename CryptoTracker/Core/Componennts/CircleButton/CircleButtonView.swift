d436af4f19729173a489c2790eaa603271341586
MinhVu <minhvulai06@gmail.com>
2024-07-13 21:58:24 +0500

    [Feature] Home Header Added

---
+++ CryptoTracker/Core/Componennts/CircleButton/CircleButtonView.swift
@@ -0,0 +1,33 @@
+//
+//  CircleButtonView.swift
+//  CryptoTracker
+//
+//  Created by Minh Vu Lai on 7/13/24.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    
+    let iconName: String
+    
+    var body: some View {
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundStyle(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundStyle(Color.theme.background)
+            )
+            .shadow(
+                color: Color.theme.accent.opacity(0.5),
+                radius: 10
+            )
+            .padding()
+    }
+}
+
+#Preview {
+    CircleButtonView(iconName: "info")
+}
---
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by Minh Vu Lai on 7/13/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            
+            // Background Layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content Layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.theme.accent)
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    HomeView()
+}
