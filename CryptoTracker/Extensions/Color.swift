8a0d75f73fc3bd034dcc1656742b0f1400b3a2d4
MinhVu <minhvulai06@gmail.com>
2024-07-13 21:33:22 +0500

    [Data] Assets Installed

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Minh Vu Lai on 7/13/24.
+//
+
+import SwiftUI
+
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
